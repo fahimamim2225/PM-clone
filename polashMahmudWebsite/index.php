4c888447f9d0038aecf660ab22aa9d2bcc141319
fahima mim <mim39853@gmail.com>
2025-02-20 19:03:39 +0600

    PM-clone

---
+++ polashMahmudWebsite/index.php
@@ -0,0 +1,19 @@
+<?php include_once('header.php'); ?>
+
+<section class="hero_area">
+    <div class="container">
+        <div class="hero_wrapper">
+            <div class="hero_text">
+                <h1><span>পিএইচপি বেসিক কোর্স</span> একটি বিগিনার ফ্রেন্ডলি পিএইচপি কোর্স</h1>
+                <p>সৌজন‍্যেঃ Learn with Polash Mahmud</p>
+                <a href="#">চলুন শুরু করি</a>
+            </div>
+            <div class="hero_img">
+                <img src="assets/images/hero.webp" alt="">
+            </div>
+        </div>
+    </div>
+</section>
+
+<?php include_once('footer.php'); ?>
+        
---
+++ polashMahmudWebsite/team.php
@@ -0,0 +1,42 @@
+<?php include_once('header.php'); ?>
+
+<div class="team_area">
+    <div class="container">
+        <div class="team_heading">
+            <h2>আমরা আছি আপনার জন্য!</h2>
+            <p>আমরা আপনাদের জন্য সর্বোচ্চ মানের কোর্স প্রদানের লক্ষ্যে অবিরাম পরিশ্রম করছি। আমাদের সাথেই থাকুন।</p>
+        </div>
+        <div class="team_wrapper">
+            <div class="single_team">
+                <div class="teamImg">
+                    <img src="assets/images/team/1.jpeg" alt="">
+                </div>
+                <div class="team_text">
+                    <h4>রাসেল আহমেদ</h4>
+                    <p>Web Developer at Get Found First</p>
+                </div>
+            </div>
+            <div class="single_team">
+                <div class="teamImg">
+                    <img src="assets/images/team/2.jpg" alt="">
+                </div>
+                <div class="team_text">
+                    <h4>পলাশ মাহমুদ</h4>
+                    <p>Owner at LWPM</p>
+                </div>
+            </div>
+            <div class="single_team">
+                <div class="teamImg">
+                    <img src="assets/images/team/3.jpg" alt="">
+                </div>
+                <div class="team_text">
+                    <h4>মোঃ নাঈমুর রহমান</h4>
+                    <p>Owner at Projukti Plus</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php include_once('footer.php'); ?>
+        
